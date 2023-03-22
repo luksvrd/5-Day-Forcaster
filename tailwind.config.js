2e4fe4d3e4d6d30bc770013b6732d0f29cf6cb54
luksvrd <ljvy9b@gmail.com>
2023-03-22 18:41:45 -0500

    refac config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 module.exports = {
-  content: ["./*.{html,js}"],
-  // safelist: [
-  //   'text-2xl',
-  //   'text-3xl',
-  //   {
-  //     pattern: /bg-(red|green|blue)-(100|200|300)/,
-  //     variants: ['lg', 'hover', 'focus', 'lg:hover'],
-  //   },
+  content: ["./index.html"],
   theme: {
     extend: {},
   },
-  plugins: [],
 };
